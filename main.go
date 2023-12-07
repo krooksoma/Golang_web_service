a5274ded7242c04122dd34cf8264c09cd28989ac
Romulo <romulojusto@gmail.com>
2023-12-07 09:15:22 -0500

    Setting up domain package

--- main.go
+++ main.go
@@ -3,19 +3,46 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"github.com/krooksoma/Golang-web-service/domain"
 )
-
-type album struct {
-	ID string `json:"id"`
-	Title string `json:"title"`
-	Artist string `json:"artist"`
-	Price float64 `json:"price"`
-}
 
 
 //get endpoint
 func getAlbums(c *gin.Context){
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+//post endpoint
+func postAlbums(c *gin.Context){
+	var newAlbum album
+
+	//Call BindJSON to bind the receiver JSON
+	//to the newAlbum
+
+	if err := c.BindJSON(&newAlbum); err != nil{
+		return
+	}
+
+	//Add the new Album to the slice
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+//get /albums/:id
+
+func getAlbumById(c *gin.Context){
+	id := c.Param("id")
+
+	//Loop over list of albuns, looking for
+	// an album whose ID value matches the parameter
+
+	for _, a := range albums{
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
 var albums = []album{
@@ -26,7 +53,9 @@ var albums = []album{
 
 func main(){
 	router := gin.Default()
-	router.get("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumById)
 
 	router.Run("localhost:8000")
 }
