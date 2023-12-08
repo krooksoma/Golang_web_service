975de685278d067740c3596d947cd23fc21ccce2
Romulo <romulojusto@gmail.com>
2023-12-08 14:42:22 -0500

    pointers in Golang

---
+++ domain-cards.go
@@ -0,0 +1,6 @@
+package main 
+
+type Card struct {
+	Value string `json:"value"`
+	Suit string `json:"suit"`
+}
--- main.go
+++ main.go
@@ -1,112 +1,30 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"github.com/gorilla/mux"
 )
 
-func homepage(w http.ResponseWriter, req *http.Request){
-	fmt.Fprintf(w, "Welcome to the Homepage")
-	fmt.Println("Endpoint Hit: homepage")
-}
-
-func returnAllArticles(w http.ResponseWriter, req *http.Request){
-	fmt.Println("Endpoint Hit: Return all Articles")
-	json.NewEncoder(w).Encode(Articles)
-}
-
-func returnSingleArticle(w http.ResponseWriter, req *http.Request){
-	fmt.Println("Endpoint Hit: Return single Article")
-	vars := mux.Vars(req)
-
-	key := vars["id"]
-
-	fmt.Fprintf(w, "Key: " + key)
-
-	for _, article := range Articles{
-		if article.Id == key{
-			json.NewEncoder(w).Encode(article)
-		}		
-	}
-	
-}
-
-func createNewArticle(w http.ResponseWriter, req *http.Request){
-	fmt.Println("Hit endpoint: Create New")
-	reqBody, _ := io.ReadAll(req.Body)
-
-	var article Article
-
-	json.Unmarshal(reqBody, &article)
-
-	Articles = append(Articles, article)
-
-	json.NewEncoder(w).Encode(article)
-}
-
-func deleteArticleById(w http.ResponseWriter, req *http.Request){
-	fmt.Println("Hit endpoint: Delete By Id")
-
-	vars := mux.Vars(req)
-	fmt.Println(vars)
-
-	key := vars["id"]
-
-	for index , article := range Articles{
-		if article.Id == key{
-			Articles = append(Articles[:index], Articles[index + 1:]...)
-		}
-	}
-}
-
-func updateArticleById(w http.ResponseWriter, req *http.Request){
-	fmt.Println("Hit endpoint: Update Article")
-	vars := mux.Vars(req)
-	reqBody, _ := io.ReadAll(req.Body)
-	key := vars["id"]
-
-	var article Article 
-
-	json.Unmarshal(reqBody, &article)
-
-	for index, article := range Articles{
-		if article.Id == key {
-			Articles = append(Articles[:index], Articles[index + 1:]...)
-			Articles = append(Articles, article)
-			json.NewEncoder(w).Encode(article)
-		}
-	}
-}
-
-func handleRequests() {
-// ###### http Implementation #######
-	// http.HandleFunc("/", homepage)
-	// http.HandleFunc("/articles", returnAllArticles)
-
-// ####### mux Implementation ########
-	//creates a new instance of mux router
-	myRouter := mux.NewRouter().StrictSlash(true)
-
-	myRouter.HandleFunc("/", homepage)
-	myRouter.HandleFunc("/articles", returnAllArticles)
-	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
-	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
-	myRouter.HandleFunc("/article/{id}", updateArticleById).Methods("PUT")
-	myRouter.HandleFunc("/article/{id}", deleteArticleById).Methods("DELETE")
-	log.Fatal(http.ListenAndServe(":8000", myRouter))
-}
 
 func main(){
-	Articles = []Article{
-		Article{Id: "1", Title: "Hello", Desc:"Article Description", Content:"Article Content"},
-		Article{Id: "2", Title: "Hello2", Desc:"Article Description2", Content:"Article Content2"},
-	}
-	fmt.Println("Rest API 2.0 = Mux Router")
-	fmt.Println("application started on port: http://localhost:8000")
-	handleRequests()
+	var jim person = person{firstName: "Jim",
+	 						lastName: "Thomas",
+	 						contact: contactInfo{phone:" 234-222-1133", zipCode:"34234"}}
+	// & gives the memory address to the variable it is pointing @
+	jimPointer := &jim
+	jimPointer.updatelastName("Jordan")
+	jim.printInformation()
+
+	//jim.updateLastName() would also work
 }
 
+func (p person) printInformation(){
+	fmt.Printf("%+v", p)
+}
+
+// *pointer get this memory address and provides value in the address
+// *<type> working with a pointer for that type
+// *<variable> workinh to manipulate the value the pointer is referencing
+func (pointerToPerson *person) updatelastName(name string){
+	fmt.Print("inside update name")
+	(*pointerToPerson).lastName = name
+}
